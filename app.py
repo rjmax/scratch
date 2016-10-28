bb3cf7dc59ccdd9970fedac4d50d4dd3fde63250
Ryan Jones (AZURE RESOURCE MANAGER) <ryjones@microsoft.com>
2016-10-28 10:29:30 -0700

    attempted sync

--- app.py
+++ app.py
@@ -5,9 +5,12 @@ This script runs the application using a development server.
 import bottle
 import os
 import sys
+from bottle import route, run, response
 
-# routes contains the HTTP handlers for our server and must be imported.
-import routes
+@route('/')
+def hello():
+    response.headers['Content-Type'] = 'application/json'
+    return "Hello World!"
 
 if '--debug' in sys.argv[1:] or 'SERVER_DEBUG' in os.environ:
     # Debug mode will enable more verbose output in the console window.
@@ -27,13 +30,6 @@ if __name__ == '__main__':
         PORT = int(os.environ.get('SERVER_PORT', '5555'))
     except ValueError:
         PORT = 5555
-
-    @bottle.route('/static/<filepath:path>')
-    def server_static(filepath):
-        """Handler for static files, used with the development server.
-        When running under a production server such as IIS or Apache,
-        the server should be configured to serve the static files."""
-        return bottle.static_file(filepath, root=STATIC_ROOT)
 
     # Starts a local test server.
     bottle.run(server='wsgiref', host=HOST, port=PORT)
